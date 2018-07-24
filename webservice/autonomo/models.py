2f8aa828466d2628f579e20a62f25954d4cf213c
Patrick <patrick.dev.atom@gmail.com>
2018-07-23 22:18:16 -0300

    Criado modelo de Autonomo e configurado Django Rest Framework.

---
+++ webservice/autonomo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Autonomo(models.Model):
+    nome = models.CharField(max_length = 120, verbose_name='Nome')
+    telefone_fixo = models.IntegerField(verbose_name='Telefone')
+    telefone_celular = models.IntegerField(verbose_name='Celular')
+    endereco = models.CharField(max_length = 150, verbose_name='Endereço')
+    cep = models.IntegerField(verbose_name='Cep')
+    cidade = models.CharField(max_length = 150, verbose_name='Cidade')
+    estado = models.CharField(max_length = 150, verbose_name='Estado')
+    email = models.CharField(max_length = 120, verbose_name='Email')
+    link_social = models.CharField(max_length = 120, verbose_name='Social')
