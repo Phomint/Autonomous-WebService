b0185ff908c136b15e55db581bc0529abe5d2a46
Patrick Amaral <patrick.dev.atom@gmail.com>
2019-02-27 14:35:39 -0300

    Novas atualizações dos modelos

--- webservice/autonomo/models.py
+++ webservice/autonomo/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 class Autonomo(models.Model):
     nome_propietario = models.CharField(max_length = 120, verbose_name='Nome')
-    telefone_comercial = models.IntegerField(verbose_name='Telefone')
+    telefone_comercial = models.CharField(max_length = 15,verbose_name='Telefone')
+    perfil_path = models.CharField(max_length = 200)
 
     servico = models.CharField(max_length = 120, verbose_name ='Serviço')
-    descricao = models.TextField(verbose_name = 'Descrição')
+    sobre = models.TextField(verbose_name = 'Sobre')
     empresa = models.CharField(max_length = 120, verbose_name = 'Empresa')
-    promo = models.CharField(max_length = 150, verbose_name = 'Promo')
+    fazer = models.TextField(verbose_name = 'Fazeres')
+    nota = models.TextField(verbose_name = 'Nota')
 
     endereco = models.CharField(max_length = 150, verbose_name='Endereço')
     cep = models.IntegerField(verbose_name='Cep')
     cidade = models.CharField(max_length = 150, verbose_name='Cidade')
     estado = models.CharField(max_length = 150, verbose_name='Estado')
     email = models.EmailField(max_length = 120, verbose_name='Email')
-    link_social = models.CharField(max_length = 120, verbose_name='Social', null=True, blank=True)
+    image_paths = models.TextField(max_length = 200)
